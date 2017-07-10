3fdac3dd70a1253a8c94ace2a5f62fec3c067939
machuang <machuang0104@126.com>
2017-07-10 23:29:30 +0800

    updage

--- part3/limits.cpp
+++ part3/limits.cpp
@@ -5,14 +5,15 @@
 int main()
 {
     using namespace std;
-    //sizeof operator yields size of type or of variable
-    cout << "int is " << sizeof(int) << " bytes.\n";
-    cout << "short is " << sizeof(short) << " bytes.\n";
-    cout << "long is " << sizeof(long) << " bytes.\n";
 
     int n_int = INT_MAX;
     short n_short = SHRT_MAX;
     long n_long = LONG_MAX;
+
+    //sizeof operator yields size of type or of variable
+    cout << "int is " << sizeof(int) << " bytes.\n";
+    cout << "short is " << sizeof(n_short) << " bytes.\n";
+    cout << "long is " << sizeof n_long << " bytes.\n";
 
     cout << "Maximun values : \n";
     cout << "int : " << n_int << endl;
